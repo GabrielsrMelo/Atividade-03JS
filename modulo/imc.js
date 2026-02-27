f4f5684d575a4dfa25815d7487ec70a917e93b0a
GABRIEL SOUSA RODRIGUES DE MELO <25203630@CFP127ALUNO.LOCAL>
2026-02-27 08:07:54 -0300

    Base criada e iniciando os calculos de IMC

---
+++ modulo/imc.js
@@ -0,0 +1,6 @@
+/*************************************************************** 
+ * Objetivo: Arquivo resposável pelos calculos de IMC de uma pessoa.
+ * Data: 27/02/2026
+ * Autor: Gabriel Sousa        
+ * Versão: 1.0
+****************************************************************/
