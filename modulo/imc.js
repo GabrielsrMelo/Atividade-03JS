ca9fd52237d99c36dc0ede883c9048e5567cf16f
GABRIEL SOUSA RODRIGUES DE MELO <25203630@CFP127ALUNO.LOCAL>
2026-02-27 10:22:23 -0300

    calcularImc ajustado e arquivo obterDados criado e iniciado

--- modulo/imc.js
+++ modulo/imc.js
@@ -7,14 +7,28 @@
 
 //Função responsável pela calculo de IMC
 function calcularImc(valor1, valor2){
-    const peso = Number(valor1)
-    const altura = Number(valor2)
+    let peso = Number(valor1)
+    let altura = Number(valor2)
 
-    const calculo = peso / (altura * altura)
-    return calculo
+    let calculo = peso / (altura * altura)
+    
+    if(calculo < 18.5){
+        console.log(`O resultado é ${calculo.toFixed(2)}! Você está abaixo do peso.`)
+    }else if(calculo >= 18.5 && calculo < 25){
+        console.log(`O resultado é ${calculo.toFixed(2)}! Você está com o peso ideal.`)
+    }else if(calculo >= 25.0 && calculo < 30){
+        console.log(`O resultado é ${calculo.toFixed(2)}! Você está Acima do peso (Sobrepeso)`)
+    }else if(calculo >= 30.0 && calculo < 35){
+        console.log(`O resultado é ${calculo.toFixed(2)}! Você está com Obsidade I`)
+    }else if (calculo >= 35 && calculo < 40){
+        console.log(`O resultado é ${calculo.toFixed(2)}! Você está com Obsidade II`)
+    }else if (calculo >= 40){
+        console.log(`O resultado é ${calculo.toFixed(2)}! Você está com Obsidade III`)
+    }else{
+        return false
+    }
 
 }
-
 module.exports={
     calcularImc
 }
